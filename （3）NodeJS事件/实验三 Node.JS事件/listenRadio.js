ef8f972248795d923c307043a558eb79e1b399d6
YangDiaa <736208296@qq.com>
2019-11-11 09:36:11 +0800

    yd

--- （3）NodeJS事件/实验三 Node.JS事件/listenRadio.js
+++
@@ -1,8 +0,0 @@
-var Radio =require("./radio.js");
-var radio=new Radio("music radio","FM 106.7");
-radio.on("play",function(){ 
-    console.log(this.name+" "+this.fm+" opened");
-    setTimeout(function(){
-        console.log("lalala...");
-    },2000)
-})
