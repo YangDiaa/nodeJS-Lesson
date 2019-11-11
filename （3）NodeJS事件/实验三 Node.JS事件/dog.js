dc3545c198bdb5898ca19d2acf4facc8199c292e
YangDiaa <736208296@qq.com>
2019-11-11 09:37:28 +0800

    yd

---
+++ （3）NodeJS事件/实验三 Node.JS事件/dog.js
@@ -0,0 +1,11 @@
+//原生模块  核心模块
+const events =require("events");
+const EventEmitter= events.EventEmitter;
+
+function Dog(name,energy){
+    EventEmitter.call(this);
+    this.name=name;
+    this.energy=energy;
+}
+Dog.prototype.__proto__=EventEmitter.prototype;
+module.exports=Dog;
